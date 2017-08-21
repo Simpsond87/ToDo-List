067e525f3818d86b940639201260ff32bd714c4b
Simpsond87 <simpson.d87@gmail.com>
2017-08-21 15:49:17 -0400

    Created ToDo-List

--- app/containers/Home/index.js
+++ app/containers/Home/index.js
@@ -11,12 +11,68 @@ import './style.css';
 import './styleM.css';
 
 export default class Home extends React.PureComponent {
+
+  constructor(){
+    super();
+    this.state = {
+      listItems:["Make Crepes", "Watch the Solar Eclipse"],
+      inputItem:""
+    }
+  }
+
+  storeItem = () => {
+
+    let listItems = this.state.listItems;
+    let inputItem = this.state.inputItem;
+
+    if(inputItem !== "")
+  {
+    listItems.push(inputItem);
+
+    this.setState({
+      listItems:listItems,
+      inputItem:""
+    })
+  }
+  }
+
+  handleItem = (event) => {
+    this.setState({
+      inputItem: event.target.value
+    })
+  }
+
+  handleEnter = (event) => {
+    console.log(event.keyCode);
+    if(event.keyCode === 13)
+    {
+        this.storeItem();
+    }
+  }
+
+  removeItem = () => {
+  }
+
   render() {
     return (
       <div className="container">
-        <Helmet title="Home" meta={[ { name: 'description', content: 'Description of Home' }]}/>
+        <Helmet title="To-Do List" meta={[ { name: 'description', content: 'Description of Home' }]}/>
 
-        //Remove this line and you can start writing your code here.
+        <div className="inputContainer">
+            <input type="text" className="todoInput" onChange={this.handleItem} onKeyDown={this.handleEnter}
+              value={this.state.inputItem}/>
+            <input type="submit" value="Add to List" className="todoButton" onClick={this.storeItem} />
+            <input type="button" value="Remove From List" className="todoButton" onClick={this.removeItem}/>
+        </div>
+
+        <div className="todoList">
+          {this.state.listItems.map((item, index) => (
+            <div className="listItem" key={index}>
+              {item}
+            </div>
+          ))}
+        </div>
+
       </div>
     );
   }
